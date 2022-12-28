b3c4996715cbf1fc15b26482cb72a0b02ce0db30
Gerson Aranibar <aranibargerson28@gmail.com>
2022-12-27 19:25:58 -0500

    update paginator

--- src/app/pages/purchases/list-purchases/list-purchases.component.ts
+++ src/app/pages/purchases/list-purchases/list-purchases.component.ts
@@ -5,8 +5,8 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
 import { PurchaseService } from 'src/app/services/purchase.service';
 import { Purchase } from 'src/app/utils/intefaces';
 import { SHARED_MODULES, TABLE_MODULES } from 'src/app/utils/modules';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
 
 const columns = ['supplier', 'amount', 'created_at'];
 
@@ -18,7 +18,6 @@ const columns = ['supplier', 'amount', 'created_at'];
 })
 export class ListPurchasesComponent implements OnInit {
   @ViewChild(MatSort) sort!: MatSort;
-  paginator!: MatPaginator;
   private purchaseService = inject(PurchaseService);
   private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
@@ -26,9 +25,9 @@ export class ListPurchasesComponent implements OnInit {
   public displayedColumns: string[] = columns;
   public dataSource!: MatTableDataSource<Purchase[]>;
 
-  public totalItems: number = 0;
-  public pageSize = 5;
   public currentPage = this.activatedRoute.snapshot.queryParams['page'] || 1;
+  public pageSize: number = 5;
+  public totalItems: number = 0;
 
   ngOnInit(): void {
     this.init_purchases(this.currentPage);
@@ -40,23 +39,25 @@ export class ListPurchasesComponent implements OnInit {
         res.docs.map((item) => (item.supplier = item.supplier.name));
         this.dataSource = new MatTableDataSource(res.docs);
         this.dataSource.sort = this.sort;
-        //this.dataSource.paginator = this.paginator;
         this.totalItems = res.totalDocs;
         this.currentPage = res.page - 1;
         this.pageSize = res.limit;
-        console.log(res);
+        this.setParams(res.page);
       },
     });
   }
 
   pageChanged(event: PageEvent) {
-    this.currentPage = event.pageIndex + 1;
+    this.currentPage = event.pageIndex;
     this.pageSize = event.pageSize;
-    this.init_purchases(this.currentPage, this.pageSize);
-    if (this.currentPage == 1) {
+    this.init_purchases(this.currentPage + 1, this.pageSize);
+  }
+
+  private setParams(page: number) {
+    if (page == 1) {
       this.router.navigate([], { queryParams: { page: null } });
     } else {
-      this.router.navigate([], { queryParams: { page: this.currentPage } });
+      this.router.navigate([], { queryParams: { page: page } });
     }
   }
 }
