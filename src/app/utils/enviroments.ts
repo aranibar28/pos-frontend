66552f9f5ea340ce9ca0b574be0127ecfac8c620
Gerson Aranibar <aranibargerson28@gmail.com>
2023-01-18 19:18:15 -0500

    update files

--- src/app/utils/enviroments.ts
+++ src/app/utils/enviroments.ts
@@ -1,9 +1,10 @@
-export const base_url = 'http://52.86.91.217/api';
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+export const base_url = proxyurl + 'http://52.86.91.217/api';
 
 export const environment = {
   production: true,
   email: 'admin@gmail.com',
-  base_url: 'http://52.86.91.217/api',
+  base_url: proxyurl + 'http://52.86.91.217/api',
   sunat: 'https://dniruc.apisperu.com/api/v1',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFyYW5pYmFyZ2Vyc29uMjhAZ21haWwuY29tIn0.ObOQyED4Nhtvm-b7bQpbxYGygJxqQqvop4VuqonMi-8',
 };
