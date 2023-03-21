68013e9572f6836ccce95c194ce1901a91ae6fab
Gerson Aranibar <aranibargerson28@gmail.com>
2023-03-21 16:07:18 -0500

    update server

--- src/app/utils/enviroments.ts
+++ src/app/utils/enviroments.ts
@@ -1,10 +1,11 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-export const base_url = proxyurl + 'http://52.86.91.217/api';
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+const production = 'http://52.86.91.217/api';
 
+export const base_url = 'http://localhost:3000/api';
 export const environment = {
-  production: true,
   email: 'admin@gmail.com',
-  base_url: proxyurl + 'http://52.86.91.217/api',
+  base_url: 'http://localhost:3000/api',
+  server: 'http://localhost:3000',
   sunat: 'https://dniruc.apisperu.com/api/v1',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFyYW5pYmFyZ2Vyc29uMjhAZ21haWwuY29tIn0.ObOQyED4Nhtvm-b7bQpbxYGygJxqQqvop4VuqonMi-8',
 };
