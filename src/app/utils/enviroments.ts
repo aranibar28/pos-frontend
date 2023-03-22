905298f298184851850ac40fde0fc0b2c821572c
Gerson Aranibar <aranibargerson28@gmail.com>
2023-03-22 09:20:52 -0500

    update files

--- src/app/utils/enviroments.ts
+++ src/app/utils/enviroments.ts
@@ -1,11 +1,8 @@
-const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-const production = 'http://52.86.91.217/api';
-
-export const base_url = 'http://localhost:3000/api';
+export const base_url = 'https://pos-lizard.fly.dev/api';
 export const environment = {
   email: 'admin@gmail.com',
-  base_url: 'http://localhost:3000/api',
-  server: 'http://localhost:3000',
+  base_url: 'https://pos-lizard.fly.dev/api',
+  server: 'https://pos-lizard.fly.dev',
   sunat: 'https://dniruc.apisperu.com/api/v1',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFyYW5pYmFyZ2Vyc29uMjhAZ21haWwuY29tIn0.ObOQyED4Nhtvm-b7bQpbxYGygJxqQqvop4VuqonMi-8',
 };
